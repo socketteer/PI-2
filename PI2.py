b019a404ce56d5f09943a905a145fbc1f9848338
eco-terrorist <distasteful.static@gmail.com>
2017-07-14 13:38:09 -0700

    calculate probability, sum probability over trials

--- PI2.py
+++ PI2.py
@@ -1,32 +1,39 @@
 import numpy as np
 
+#make better version with matrices later
 
-def cost(param):
-    cost = 0
+def cost(trajectory):
+    cost = 1
     #run trial, add to cost
     return cost
 
-def probability():
-    pass
+def probability(trajectories, num_params, sensitivity):
+    prob = np.zeros(num_params)
+    prob_sum = 0
+    #trajectories with lower costs have higher probabilities
+    for k in range (num_params):
+        prob[k] = (np.exp(-1/sensitivity * cost(trajectories[k])))
+        prob_sum += prob[k]
+    return prob / prob_sum
 
-def add_noise(param, num_params, variance):
-    noise = np.random.normal(0,np.sqrt(variance),num_params)
-    for k in range(num_params):
-        param[k] += noise[k]
-        
+def generate_noise(num_traj, num_params, variance):
+    return np.random.normal(0,np.sqrt(variance),(num_traj, num_params))
+                
+def avg_over_trials(num_traj, num_params, prob, noise):
+    avg = np.zeros(num_params)
+    for k in range(num_traj):
+        avg += prob[k]*noise[k:]
+    return avg    
 
-K = 10
+num_traj = 10
 num_params = 12
+sensitivity = 1
+prob = np.zeros(num_params)
+base_trajectory = np.zeros(num_params) 
+variance = 1
+noise = generate_noise(num_traj, num_params, variance)
 
-#cost-to-go = []
-prob = []
-param = np.zeroes((K, num_params))
-
-#for k in range (K):
-#    cost-to-go.append(Cost(param[k:])) #necessary?
-    
-    
-for k in range (K):
-    prob.append(probability(cost(param[k:])))
-    
+for k in range (num_traj):
+    #find probabilities of trajectories with gaussian noise
+    prob[k] = (probability(base_trajectory + noise[k:], num_params, sensitivity))
     
