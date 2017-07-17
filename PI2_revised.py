4033e2d7953643887bafbb12c0a212c8ee11e9c3
eco-terrorist <distasteful.static@gmail.com>
2017-07-17 13:32:57 -0700

    implemented averaging over time-steps

--- PI2_revised.py
+++ PI2_revised.py
@@ -3,7 +3,7 @@ import numpy as np
     
 def cost():
     #calculate cost using state information
-    return 0    
+    return 0 #returns scalar cost-to-go for timestep   
     
 def rollout(dmp, start_params, num_params, num_activation, timesteps, prob, sensitivity, M, R):
     #cost = np.zeros(timesteps)
@@ -24,26 +24,34 @@ def rollout(dmp, start_params, num_params, num_activation, timesteps, prob, sens
         M[i] = np.transpose(R)*dmp.goals()*np.transpose(dmp.goals()) \
             / (np.transpose(dmp.goals())*np.transpose(R)*dmp.goals())
         
-    prob /= prob_sum     
+    prob /= prob_sum #prob is 1D array here   
     
 def probability(sensitivity):
-    return np.exp(-1/sensitivity * cost())
+    return np.exp(-1/sensitivity * cost()) #returns scalar
     
 def generate_noise(num_traj, num_params, num_activation, variance):
-    return np.random.normal(0,np.sqrt(variance),(num_traj, num_params, num_activation)
+    return np.random.normal(0,np.sqrt(variance),(num_traj, num_params, num_activation)) #returns 3D array
 
 def avg_over_trials(num_traj, num_params, num_activation, prob, noise, M):
-    avg = np.zeros(num_params, num_activation)
+    avg = np.zeros((num_params, num_activation))
     #for each timestep i
     for p in range(num_params): #for each parameter
         for k in range(num_traj): #loop through trajectories
             #incrementing average by noise of parameter weighted by probability of that trajectory
-            avg[p,:] += prob[k] * M[k] * noise[k,p,:]
-        
-    return avg    
+            avg[p,:] += prob[k] * M[k] * noise[k,p,:]        
+    return avg #returns 2D array   
 
-def avg_over_timesteps(num_params, timesteps):
-    pass
+def avg_over_timesteps(dmp, num_params, num_activation, timesteps, timestep_param_update):
+    avg = np.zeros((num_params, num_activation))
+    denominator = 0 #to scale avg
+    for i in range(timesteps - 1): #for each timestep
+        for j in range(num_activation):
+            #param update for basis j  for all dmp dimensions incremented by timestep param update weighted by corresponding activation  
+            avg[:,j] += (timesteps - i) * dmp.get_activations(activation = j, time = i) * \
+                timestep_param_update[i,:,j]     
+            denominator += dmp.get_activations(activation = j, time = i) * (timesteps - i)
+                                                               
+    return avg / denominator #returns 2D array
 
 
 timesteps = 30
@@ -53,7 +61,7 @@ num_activation = 10
 sensitivity = 1
 prob = np.zeros((num_traj, timesteps))
 base_trajectory = np.zeros((num_params, num_activation)) 
-noise = generate_noise(num_traj, num_params, variance)
+noise = generate_noise(num_traj, num_params, num_activations, variance)
 timestep_param_update = np.zeros((timesteps, num_params, num_activation))
 param_update = np.zeros((num_params, num_activation))
 control_ratio = 0.7
@@ -66,8 +74,10 @@ for k in range (num_traj):
     rollout(start_params = base_trajectory + noise[k,:,:], timesteps, prob[k:], sensitivity, M[k:], R)
     
 for i in range (timesteps):
-    timestep_param_update[i,:,:] = avg_over_trials(num_traj, num_params, prob[:,i], noise, M[:i])
+    timestep_param_update[i,:,:] = avg_over_trials(num_traj, num_params, num_activation, \
+        prob[:,i], noise, M[:i])
     
-param_update = avg_over_timesteps(num_params, timesteps) 
+param_update = avg_over_timesteps(dmp, num_params, num_activation, timesteps, timestep_param_update) 
 
-    
+new_params = base_trajectory + param_update #adding 2D arrays
+
